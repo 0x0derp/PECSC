380662ad35896a660619bdb18f221fccebaf7bf1
derp <0x0derp@mail.com>
2015-12-14 06:18:06 -0500

    Update pecsc_Server.py

--- pecsc_Server.py
+++ pecsc_Server.py
@@ -1,1 +1,28 @@
+import sys
+import socket
 
+HOST = raw_input("Server IP: ")
+PORT = input("Server Port: ")
+MAXCLI = input("Maximum number of Clients: ")
+userList = ["firstip"]
+
+def updateClientList():
+  print "update client list"
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.bind((HOST, PORT))
+sock.listen(MAXCLI)
+
+for index in range(MAXCLI):
+  conn, addr = sock.accept()
+  print "Connection by: ", addr
+  userList[index] = addr
+  
+# echo users' messages to each other (like a chatroom hopefully)
+while True:
+  retrieveMSG = sock.recv(1026)
+  print retrieveMSG
+  if retrieveMSG == "killserver":
+    sys.exit(0)
+  for ip in userList:
+    sock.sendto(retrieveMSG, ip)
